8077dd39e54ae80b29264030724bf8106deffeb0
Stefan Heule <stefanheule@gmail.com>
2013-01-18 12:51:35 +0100

    Improve VerificationError.

--- src/main/scala/semper/sil/verifier/VerificationError.scala
+++ src/main/scala/semper/sil/verifier/VerificationError.scala
@@ -1,7 +1,21 @@
 package semper.sil.verifier
 
+import semper.sil.ast.source.SourceLocation
+import semper.sil.ast.expressions.terms.FieldReadExpression
+import semper.sil.ast.ASTNode
+
 /** A case class to describe an error during the verification of a SIL program.
   *
   * @author Stefan Heule
   */
-sealed case class VerificationError(id: String, lineNr: Int)
+abstract class VerificationError(val id: String, val offendingNode: ASTNode) {
+  def readableMessage(): String
+  def location: SourceLocation = offendingNode.sourceLocation
+}
+
+// Example of an error (not used yet):
+case class NullFieldRead(fieldAccess: FieldReadExpression) extends VerificationError("null.field.read", fieldAccess) {
+  def readableMessage = {
+    "The receiver in the expression %s might be null.".format(fieldAccess)
+  }
+}
