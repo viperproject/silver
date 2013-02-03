216553bcfb67f18d79f4176c38d54fa730afac08
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2013-02-03 22:52:46 +0100

    Made ProgramVariable a case class to get structural equality

--- src/main/scala/semper/sil/ast/programs/symbols/ProgramVariable.scala
+++ src/main/scala/semper/sil/ast/programs/symbols/ProgramVariable.scala
@@ -3,19 +3,11 @@ package semper.sil.ast.programs.symbols
 import semper.sil.ast.types.DataType
 import semper.sil.ast.source.SourceLocation
 
-class ProgramVariable(
-                       val name: String,
-                       val dataType: DataType
-                       )(val sourceLocation: SourceLocation, val comment: List[String]) extends Variable {
+case class ProgramVariable(val name: String,
+                           val dataType: DataType)
+                          (val sourceLocation: SourceLocation,
+                           val comment: List[String])
+    extends Variable {
 
   override def toString: String = name
-
-  override def equals(other: Any): Boolean = {
-    other match {
-      case v: ProgramVariable => this eq v
-      case _ => false
-    }
-  }
-
-  override def hashCode(): Int = name.hashCode()
 }
