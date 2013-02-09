2e47aaad5024dbb79ec84ce103cf7410c8528df9
Stefan Heule <stefanheule@gmail.com>
2013-02-09 02:24:12 +0100

    Remove old SIL AST.

--- src/main/scala/semper/sil/ast/ASTNode.scala
+++
@@ -1,8 +0,0 @@
-package semper.sil.ast
-
-import source.SourceLocation
-
-abstract class ASTNode protected[sil] {
-  def sourceLocation: SourceLocation
-  def comment: scala.collection.immutable.List[String] //comments
-}
