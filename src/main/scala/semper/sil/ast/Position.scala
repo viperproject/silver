071b120eef466bb3491293985d883535e7a9a5e5
Bernhard F. Brodowsky <brodowsb@ethz.ch>
2013-02-19 22:45:39 +0100

    Created TranslatedPosition

--- src/main/scala/semper/sil/ast/Position.scala
+++ src/main/scala/semper/sil/ast/Position.scala
@@ -6,5 +6,17 @@ sealed trait Position
 /** Describes ''no location'' for cases where a `Position` is expected, but not available. */
 case object NoPosition extends Position
 
+/** An actual position that has a line and column. */
+trait RealPosition {
+  def line: Int
+  def column: Int
+}
+
 /** Describes a location in a file by line and column number. */
-case class SourcePosition(line: Int, column: Int) extends Position
+case class SourcePosition(line: Int, column: Int) extends Position with RealPosition
+
+/** Refers to a location in a source language that has been translated to SIL. */
+case class TranslatedPosition(pos: RealPosition) extends Position with RealPosition {
+  def line = pos.line
+  def column = pos.column
+}
