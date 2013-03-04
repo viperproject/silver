9c4b8200d58cf9af8918492784a23ee07236684b
Stefan Heule <stefanheule@gmail.com>
2013-03-03 21:40:31 -0500

    Make it possible to set command-line options after the verifier has been created.

--- src/main/scala/semper/sil/verifier/Verifier.scala
+++ src/main/scala/semper/sil/verifier/Verifier.scala
@@ -5,16 +5,21 @@ import semper.sil.ast.Program
 
 /** An abstract class for verifiers of SIL programs.
   *
-  * @param options The command-line arguments to be used in this verifier.
   * @author Stefan Heule
   */
-abstract class Verifier(options: Seq[String] = Nil) {
+abstract class Verifier() {
+
+  /** Set the command-line arguments to be used in this verifier. */
+  def commandLineArgs(options: Seq[String])
 
   /** The name of this verifier (all-lowercase, to be used to uniquely identify this verifier). */
   def name: String
 
   /** Returns the version of the verifier. */
   def version: String
+
+  /** Returns the copyright string of this verifier, e.g., "(c) 2013 Name" */
+  def copyright: String
 
   /** Returns the versions of its dependencies.
     *
