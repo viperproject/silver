3f95e1938478fda70acbb94346fb091e5d474b51
Stefan Heule <stefanheule@gmail.com>
2013-03-27 03:45:43 +0100

    Extend parse AST with visitors.

--- src/main/scala/semper/sil/parser/ParseAst.scala
+++ src/main/scala/semper/sil/parser/ParseAst.scala
@@ -6,8 +6,58 @@ import org.kiama.attribution.Attributable
 /**
  * The root of the parser abstract syntax tree.  Note that we prefix all nodes with `P` to avoid confusion
  * with the actual SIL abstract syntax tree.
+ *
+ * @author Stefan Heule
  */
-sealed trait PNode extends Positioned with Attributable
+sealed trait PNode extends Positioned with Attributable {
+  /**
+   * Returns a list of all direct sub-nodes of this node.
+   */
+  def subnodes = Nodes.subnodes(this)
+
+  /**
+   * Applies the function `f` to the node and the results of the subnodes.
+   */
+  def reduce[T](f: (PNode, Seq[T]) => T) = Visitor.reduce[T](this)(f)
+
+  /**
+   * More powerful version of reduce that also carries a context argument through the tree.
+   */
+  def reduce[C, R](context: C, enter: (PNode, C) => C, combine: (PNode, C, Seq[R]) => R) = {
+    Visitor.reduce[C, R](this)(context, enter, combine)
+  }
+
+  /**
+   * Applies the function `f` to the AST node, then visits all subnodes.
+   */
+  def visit(f: PNode => Unit) {
+    Visitor.visit(this)(f)
+  }
+
+  /**
+   * Applies the function `f1` to the AST node, then visits all subnodes,
+   * and finally calls `f2` to the AST node.
+   */
+  def visit(n: PNode, f1: PNode => Unit, f2: PNode => Unit) {
+    Visitor.visit(this, f1, f2)
+  }
+
+  /**
+   * Applies the function `f` to the AST node, then visits all subnodes if `f`
+   * returned true.
+   */
+  def visitOpt(n: PNode)(f: PNode => Boolean) {
+    Visitor.visitOpt(this)(f)
+  }
+
+  /**
+   * Applies the function `f1` to the AST node, then visits all subnodes if `f1`
+   * returned true, and finally calls `f2` to the AST node.
+   */
+  def visitOpt(n: PNode, f1: PNode => Boolean, f2: PNode => Unit) {
+    Visitor.visitOpt(this, f1, f2)
+  }
+}
 
 // Identifiers (uses and definitions)
 trait Identifier
@@ -55,15 +105,127 @@ case class PExhale(e: PExp) extends PStmt
 case class PInhale(e: PExp) extends PStmt
 case class PVarAssign(idnuse: PIdnUse, rhs: PExp) extends PStmt
 case class PIf(cond: PExp, thn: PStmt, els: PStmt) extends PStmt
-case class PWhile(cond: PExp, invs: Seq[PExp], stmt: PStmt) extends PStmt
+case class PWhile(cond: PExp, invs: Seq[PExp], body: PStmt) extends PStmt
 case class PLocalVarDecl(idndef: PIdnDef, typ: PType, init: Option[PExp]) extends PStmt
 case class PFreshReadPerm(vars: Seq[PIdnUse], stmt: PStmt) extends PStmt
 
 // Program
-sealed trait PProgram extends PNode
+case class PProgram(idndef: PIdnDef, methods: Seq[PMethod]) extends PNode
 
 // Method
 case class PMethod(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], formalReturns: Seq[PFormalArgDecl], pres: Seq[PExp], posts: Seq[PExp], body: PStmt) extends PNode
 
 // Domain
-sealed trait PDomain extends PNode
+case class PDomain(idndef: PIdnDef) extends PNode
+
+
+/**
+ * Utility methods for parser parserAST nodes.
+ *
+ * @author Stefan Heule
+ */
+object Nodes {
+
+  /**
+   * See PNode.subnodes.
+   */
+  def subnodes(n: PNode): Seq[PNode] = {
+    n match {
+      case PIdnDef(name) => Nil
+      case PIdnUse(name) => Nil
+      case PFormalArgDecl(idndef, typ) => Seq(idndef, typ)
+      case PPrimitiv(name) => Nil
+      case PTypeVar(name) => Nil
+      case PDomainType(domain, args) => Seq(domain) ++ args
+      case PUnkown() => Nil
+      case PBinExp(left, op, right) => Seq(left, right)
+      case PUnExp(op, exp) => Seq(exp)
+      case PIntLit(i) => Nil
+      case PBoolLit(b) => Nil
+      case PResultLit() => Nil
+      case PFieldAcc(rcv, field) => Seq(rcv, field)
+      case PSeqn(ss) => ss
+      case PFold(exp) => Seq(exp)
+      case PUnfold(exp) => Seq(exp)
+      case PExhale(exp) => Seq(exp)
+      case PInhale(exp) => Seq(exp)
+      case PVarAssign(target, rhs) => Seq(target, rhs)
+      case PIf(cond, thn, els) => Seq(cond, thn, els)
+      case PWhile(cond, invs, body) => Seq(cond) ++ invs ++ Seq(body)
+      case PLocalVarDecl(idndef, typ, init) => Seq(idndef, typ) ++ (if (init.isDefined) Seq(init.get) else Nil)
+      case PFreshReadPerm(vars, stmt) => vars ++ Seq(stmt)
+      case PProgram(idndef, methods) => Seq(idndef) ++ methods
+      case PDomain(idndef) => Seq(idndef)
+      case PMethod(idndef, args, rets, pres, posts, body) => Seq(idndef) ++ args ++ rets ++ pres ++ posts ++ Seq(body)
+    }
+  }
+}
+
+/**
+ * An implementation for visitors of the SIL AST.
+ *
+ * @author Stefan Heule
+ */
+object Visitor {
+
+  /**
+   * See Node.reduce.
+   */
+  def reduce[T](n: PNode)(f: (PNode, Seq[T]) => T): T = {
+    val subResults = n.subnodes.map(reduce[T](_)(f))
+    f(n, subResults)
+  }
+
+  /**
+   * See Node.reduce.
+   */
+  def reduce[C, R](n: PNode)(context: C, enter: (PNode, C) => C, combine: (PNode, C, Seq[R]) => R): R = {
+    val newContext = enter(n, context)
+    val subResults = n.subnodes.map(reduce[C, R](_)(newContext, enter, combine))
+    combine(n, context, subResults)
+  }
+
+  /**
+   * See Node.visit.
+   */
+  def visit(n: PNode)(f: PNode => Unit) {
+    f(n)
+    for (sub <- n.subnodes) {
+      visit(sub)(f)
+    }
+  }
+
+  /**
+   * See Node.visit.
+   */
+  def visit(n: PNode, f1: PNode => Unit, f2: PNode => Unit) {
+    f1(n)
+    for (sub <- n.subnodes) {
+      visit(sub, f1, f2)
+    }
+    f2(n)
+  }
+
+  /**
+   * See Node.visitOpt.
+   */
+  def visitOpt(n: PNode)(f: PNode => Boolean) {
+    if (f(n)) {
+      for (sub <- n.subnodes) {
+        visitOpt(sub)(f)
+      }
+    }
+  }
+
+  /**
+   * See Node.visitOpt.
+   */
+  def visitOpt(n: PNode, f1: PNode => Boolean, f2: PNode => Unit) {
+    if (f1(n)) {
+      for (sub <- n.subnodes) {
+        visitOpt(sub, f1, f2)
+      }
+    }
+    f2(n)
+  }
+}
