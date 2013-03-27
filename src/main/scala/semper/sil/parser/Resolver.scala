7507401b46b2f9fc030d663f87588564a150e371
Stefan Heule <stefanheule@gmail.com>
2013-03-27 14:22:26 +0100

    Simplify resolver.

--- src/main/scala/semper/sil/parser/Resolver.scala
+++ src/main/scala/semper/sil/parser/Resolver.scala
@@ -4,34 +4,26 @@ import org.kiama.util.Messaging.message
 import org.kiama.util.Messaging.messagecount
 
 /**
- * A resolver and type-checker for our intermediate SIL AST.  The approach is based on the example
- * called oberon0 from kiama.
+ * A resolver and type-checker for the intermediate SIL AST.
  */
 object Resolver {
-  def analyser = Seq(NameAnalyser())
+  val names = NameAnalyser()
 
   def run(p: PProgram) {
-    analyser foreach {
-      a => a.run(p)
-      if (messagecount > 0) return
+    if (names.run(p)) {
+
     }
   }
 }
 
-trait Analyser {
-
-  /**
-   * Run the resolver, reporting any potential errors.
-   */
-  def run(p: PProgram)
-
-}
-
-case class NameAnalyser() extends Analyser with Environment {
+/**
+ * Resolves identifiers to their declaration.
+ */
+case class NameAnalyser() extends Environments {
 
   private val idnMap = collection.mutable.HashMap[String, Entity]()
 
-  def run(p: PProgram) {
+  def run(p: PProgram): Boolean = {
     // find all declarations
     p.visit {
       case i@PIdnDef(name) =>
@@ -58,10 +50,55 @@ case class NameAnalyser() extends Analyser with Environment {
       case i@PIdnUse(name) =>
         idnMap.getOrElse(name, UnknownEntity()) match {
           case UnknownEntity() =>
-            message(i, s"$name has not been defined.")
+            message(i, s"$name not defined.")
           case _ =>
         }
       case _ =>
     }
+    messagecount == 0
   }
+}
+
+/**
+ * General implementation of environments as stacked scopes.
+ */
+trait Environments {
+
+  /** An entity is some kind of definition */
+  sealed trait Entity
+  case class LocVarEnt(decl: PLocalVarDecl) extends NamedEntity {
+    val name = decl.idndef.name
+  }
+  case class MethodEnt(decl: PMethod) extends NamedEntity {
+    val name = decl.idndef.name
+  }
+  case class FormalArgEnt(decl: PFormalArgDecl) extends NamedEntity {
+    val name = decl.idndef.name
+  }
+  case class ProgramEnt(decl: PProgram) extends NamedEntity {
+    val name = decl.idndef.name
+  }
+
+  /**
+   * A named entity.
+   */
+  sealed trait NamedEntity extends Entity
+
+  /**
+   * An entity that represents an error situation.  These entities are
+   * usually accepted in most situations to avoid cascade errors.
+   */
+  abstract class ErrorEntity(name: String) extends Entity
+
+  /**
+   * A entity represented by names for whom we have seen more than one
+   * declaration so we are unsure what is being represented.
+   */
+  case class MultipleEntity() extends ErrorEntity("multiple")
+
+  /**
+   * An unknown entity, represented by names whose declarations are missing.
+   */
+  case class UnknownEntity() extends ErrorEntity("unknown")
+
 }
