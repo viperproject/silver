5e14677e49bff39750725bb1f882308bdfa809c4
Bernhard F. Brodowsky <brodowsb@ethz.ch>
2013-04-19 15:41:17 +0200

    Changed the way positions are displayed.

--- src/main/scala/semper/sil/ast/Position.scala
+++ src/main/scala/semper/sil/ast/Position.scala
@@ -14,7 +14,7 @@ case object NoPosition extends Position {
 trait RealPosition {
   def line: Int
   def column: Int
-  override def toString = line.toString
+  override def toString = s"$line.$column"
 }
 object RealPosition {
   def unapply(pos: RealPosition) = Some(pos.line, pos.column)
@@ -27,5 +27,5 @@ case class SourcePosition(line: Int, column: Int) extends Position with RealPosi
 case class TranslatedPosition(file: File, pos: RealPosition) extends Position with RealPosition {
   val line = pos.line
   val column = pos.column
-  override def toString = s"$file:$line"
+  override def toString = s"${file.getName}:$line"
 }
