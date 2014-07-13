6f4f67610a3d47ea3a52fd2d550a2a2fe0455074
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2014-07-13 17:08:47 +0200

    All ghost operations other than unfolding are considered impure per default

--- src/main/scala/semper/sil/ast/utility/Expressions.scala
+++ src/main/scala/semper/sil/ast/utility/Expressions.scala
@@ -12,7 +12,8 @@ object Expressions {
     case InhaleExhaleExp(in, ex) => isPure(in) && isPure(ex)
     case BinExp(e0, e1) => isPure(e0) && isPure(e1)
     case CondExp(cnd, thn, els) => isPure(cnd) && isPure(thn) && isPure(els)
-    case gop: GhostOperation => isPure(gop.body)
+    case unf: Unfolding => isPure(unf.body)
+    case gop: GhostOperation => false
     case QuantifiedExp(_, e0) => isPure(e0)
 
     case _: Literal
