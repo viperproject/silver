039f3e2b3b0533024f1329a1b804652fe2e13dc2
Alexander J. Summers <alexander.summers@inf.ethz.ch>
2015-08-11 22:28:59 -0400

    refactored purify as asBooleanExp, and added an appropriate case for Unfolding nodes.

--- src/main/scala/viper/silver/ast/utility/Expressions.scala
+++ src/main/scala/viper/silver/ast/utility/Expressions.scala
@@ -42,9 +42,12 @@ object Expressions {
     case fapp: FuncApp if fapp.func(p).pres.exists(isHeapDependent(_, p)) =>
   }
 
-  def purify(e: Exp): Exp = e.transform({
+  def asBooleanExp(e: Exp): Exp = {
+    e.transform({
       case _: AccessPredicate => TrueLit()()
+      case Unfolding(predicate, exp) => asBooleanExp(exp)
     })()
+  }
 
   def whenInhaling(e: Exp) = e.transform()(post = {
     case InhaleExhaleExp(in, _) => in
@@ -159,7 +162,7 @@ object Expressions {
     // We want to make the proof obligations as weak as possible, but we cannot use access predicates as guards,
     // so we need to remove them and make the guard weaker. This makes the proof obligations slightly too strong,
     // but it is the best we can do.
-    val guard = purify(left)
+    val guard = asBooleanExp(left)
     reduceLazyBinOpProofObs(guard, leftConds, rightConds, p)
   }
 
