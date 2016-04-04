8f7b8be8dba8fddf41678de252bd07c481f68b14
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2016-04-04 12:55:11 +0200

    Fixed an issue with quantifiers under magic wands that crashed Silicon and Carbon (see also Silicon issue 225)

--- src/main/scala/viper/silver/ast/Expression.scala
+++ src/main/scala/viper/silver/ast/Expression.scala
@@ -124,10 +124,11 @@ case class MagicWand(left: Exp, right: Exp)(val pos: Position = NoPosition, val 
   // maybe rename this sometime
   def subexpressionsToEvaluate(p: Program): Seq[Exp] = {
     this.shallowCollect {
-      case old: Old => old
-      case lo: LabelledOld => lo
-      case e: Exp if !e.isHeapDependent(p) => e
-    }
+      case old: Old => Some(old)
+      case lo: LabelledOld => Some(lo)
+      case q: QuantifiedExp if q.isHeapDependent(p) => None
+      case e: Exp if !e.isHeapDependent(p) => Some(e)
+    }.flatten
   }
 
   def structurallyMatches(other: MagicWand, p: Program): Boolean = {
