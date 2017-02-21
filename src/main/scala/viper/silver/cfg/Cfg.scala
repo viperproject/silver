78f5e16e7e0dcbd9c5384212e9a970ec610adab6
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2017-02-21 14:25:21 +0100

    Removed an illegal character

--- src/main/scala/viper/silver/cfg/Cfg.scala
+++ src/main/scala/viper/silver/cfg/Cfg.scala
@@ -158,7 +158,7 @@ trait Cfg[S, E] {
       case PostconditionBlock(posts) =>
         block.toString + "|" + posts.map(_.toString).map(escape).mkString("|")
       case LoopHeadBlock(invs, stmts) =>
-§        block.toString + "|" + invs.map(inv => "invariant " + escape(inv.toString)).mkString("|") + "|" + stmts.map(_.toString).map(escape).mkString("|")
+        block.toString + "|" + invs.map(inv => "invariant " + escape(inv.toString)).mkString("|") + "|" + stmts.map(_.toString).map(escape).mkString("|")
       case ConstrainingBlock(_, _) =>
         block.toString
     }
