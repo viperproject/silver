c5732a6748d5525c5576d8102ce93074bc9b9ae5
Simon Fritsche <simon.fritsche+bitbucket@gmail.com>
2017-03-20 16:15:38 +0100

    Correcting merge mistake with Revision:2548

--- src/main/scala/viper/silver/ast/Program.scala
+++ src/main/scala/viper/silver/ast/Program.scala
@@ -298,13 +298,6 @@ case class DomainFunc(name: String, formalArgs: Seq[LocalVarDecl], typ: Type, un
 sealed trait Member extends Node with Positioned with Infoed with TransformableErrors {
   require(Consistency.validUserDefinedIdentifier(name))
   def name: String
-
-  // we override the definition of hashCode/equals to avoid unbounded recursion
-  override def hashCode = name.hashCode
-  override def equals(o: Any) = o match {
-    case m: Member => name == m.name
-    case _ => false
-  }
 }
 
 /** Common ancestor for domain members. */
@@ -316,14 +309,6 @@ sealed trait DomainMember extends Node with Positioned with Infoed with Transfor
 
   /** See [[viper.silver.ast.utility.Types.freeTypeVariables]]. */
   lazy val freeTypeVariables = Types.freeTypeVariables(this)
-
-  // we override the definition of hashCode/equals to avoid unbounded recursion
-  override def hashCode = name.hashCode
-
-  override def equals(o: Any) = o match {
-    case m: DomainMember => name == m.name
-    case _ => false
-  }
 }
 
 /** Common ancestor for things with formal arguments. */
