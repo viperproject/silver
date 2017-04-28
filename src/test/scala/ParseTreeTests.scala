b6492ea90a5fa3a9d485d43c5c0305120158babd
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2017-04-28 16:56:13 +0200

    Various improvements/fixes to the unit tests:  - refactored commonly used operations into TestHelpers.scala  - build.sbt:      * disabled running tests in parallel (caused nondeterministic test failures; resolver/typechecker seems to not be thread-safe)      * pre-load SLF4J logger factory to prevent racy initialisation problems of Logback  - disabled a test for the domain instance generation code (looks plain wrong)  - renamed a misspelled test directory (hygienicMacros)  - worked around a nondeterminism issue in a regex rewriter test

--- src/test/scala/ParseTreeTests.scala
+++ src/test/scala/ParseTreeTests.scala
@@ -4,37 +4,31 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.Paths
 
-import org.scalatest.{FunSuite, Matchers}
+import TestHelpers.MockSilFrontend
+import org.scalatest.FunSuite
 import viper.silver.ast._
-import viper.silver.frontend.{SilFrontend, TranslatorState}
-import viper.silver.parser._
-import viper.silver.verifier.AbstractError
 
 import scala.language.implicitConversions
 
-
 class ParseTreeTests extends FunSuite {
-
-
   test("CyclicMacroDetection") {
-    val filePrefix = "parsertests\\cyclicMacros\\"
+    val filePrefix = "parsertests/cyclicMacros/"
     val files = Seq("simple", "complex", "complexExp")
 
-    val frontend = new CustomFrontend
+    val frontend = new MockSilFrontend
 
     files foreach {
       fileName => {
         val fileRes = getClass.getResource(filePrefix + fileName + ".sil")
         assert(fileRes != null, s"File $filePrefix$fileName not found")
         val file = Paths.get(fileRes.toURI)
-        var targetNode: Node = null
 
         frontend.translate(file) match {
-          case (Some(p), _) => println("Everything ok, but we expected cyclic error!"); assert(false)
+          case (Some(_), _) =>
+            fail("Expected cyclic macro errors, but gone none")
           case (None, errors) => errors.foreach(e => {
-            println("Error: " + e)
             assert(e.readableMessage.contains("Recursive macro declaration found"))
           })
         }
@@ -43,143 +37,66 @@ class ParseTreeTests extends FunSuite {
   }
 
   test("MacroExpansion") {
-    val filePrefix = "parsertests\\macroExpansion\\"
+    val filePrefix = "parsertests/macroExpansion/"
     val files = Seq("simple", "simple2", "simpleExp", "simpleArgs", "simpleArgs2", "simpleArgsExp", "simpleMethod", "simpleMethodExp")
-    //val files = Seq("simpleMethodExp")
 
-    val frontend = new CustomFrontend
+    val frontend = new MockSilFrontend
 
-    files foreach {
-      fileName => {
-        val fileRes = getClass.getResource(filePrefix + fileName + ".sil")
-        assert(fileRes != null, s"File $filePrefix$fileName" + ".sil not found")
-        val file = Paths.get(fileRes.toURI)
-
-        val fileRef = getClass.getResource(filePrefix + fileName + "Ref" + ".sil")
-        assert(fileRef != null, s"File $filePrefix$fileName" + "Ref.sil not found")
-        val fileR = Paths.get(fileRef.toURI)
-
-        var targetNode: Node = null
-        var targetRef: Node = null
-
-        frontend.translate(file) match {
-          case (Some(p), _) => targetNode = p
-          case (None, errors) => println("error occured during translating: " + errors)
-        }
-
-        frontend.translate(fileR) match {
-          case (Some(p), _) => targetRef = p
-          case (None, errors) => println("error occured during translating: " + errors)
-        }
-
-        println("New: " + targetNode.toString())
-        println("Reference: " + targetRef.toString())
-        assert(targetNode.toString() == targetRef.toString(), "Files are not equal")
-      }
-    }
-
+    files foreach(fileName =>
+      parseAndCompare(filePrefix + fileName + ".sil", filePrefix + fileName + "Ref" + ".sil", frontend))
   }
 
-  test("HygenicMacros") {
-    val filePrefix = "parsertests\\hygenicMacros\\"
+  test("HygienicMacros") {
+    val filePrefix = "parsertests/hygienicMacros/"
     val files = Seq("simple", "nested", "collision", "collision2", "forall", "loopConstruction")
-    //val files = Seq("loopConstruction")
 
-    val frontend = new CustomFrontend
+    val frontend = new MockSilFrontend
 
-    files foreach {
-      fileName => {
-        val fileRes = getClass.getResource(filePrefix + fileName + ".sil")
-        assert(fileRes != null, s"File $filePrefix$fileName" + ".sil not found")
-        val file = Paths.get(fileRes.toURI)
-
-        val fileRef = getClass.getResource(filePrefix + fileName + "Ref" + ".sil")
-        assert(fileRef != null, s"File $filePrefix$fileName" + "Ref.sil not found")
-        val fileR = Paths.get(fileRef.toURI)
-
-        var targetNode: Node = null
-        var targetRef: Node = null
-
-        frontend.translate(file) match {
-          case (Some(p), _) => targetNode = p
-          case (None, errors) => println("error occured during translating: " + errors)
-        }
-
-        frontend.translate(fileR) match {
-          case (Some(p), _) => targetRef = p
-          case (None, errors) => println("error occured during translating: " + errors)
-        }
-
-        println("New: " + targetNode.toString())
-        println("Reference: " + targetRef.toString())
-        assert(targetNode.toString() == targetRef.toString(), "Files are not equal")
-      }
-    }
-
+    files foreach (fileName =>
+      parseAndCompare(filePrefix + fileName + ".sil", filePrefix + fileName + "Ref" + ".sil", frontend))
   }
-
 
   test("Imports") {
-    val filePrefix = "parsertests\\imports\\"
+    val filePrefix = "parsertests/imports/"
     val files = Seq("simple", "complex", "cyclic")
 
-    val frontend = new CustomFrontend
+    val frontend = new MockSilFrontend
 
-    files foreach {
-      fileName => {
-        val fileRes = getClass.getResource(filePrefix + fileName + ".sil")
-        assert(fileRes != null, s"File $filePrefix$fileName" + ".sil not found")
-        val file = Paths.get(fileRes.toURI)
-
-        val fileRef = getClass.getResource(filePrefix + fileName + "Ref" + ".sil")
-        assert(fileRef != null, s"File $filePrefix$fileName" + "Ref.sil not found")
-        val fileR = Paths.get(fileRef.toURI)
-
-        var targetNode: Node = null
-        var targetRef: Node = null
-
-        frontend.translate(file) match {
-          case (Some(p), _) => targetNode = p
-          case (None, errors) => println("error occured during translating: " + errors)
-        }
-
-        frontend.translate(fileR) match {
-          case (Some(p), _) => targetRef = p
-          case (None, errors) => println("error occured during translating: " + errors)
-        }
-
-        println("New: " + targetNode.toString())
-        println("Reference: " + targetRef.toString())
-        assert(targetNode.toString() == targetRef.toString(), "Files are not equal")
-      }
-    }
-
+    files foreach (fileName =>
+      parseAndCompare(filePrefix + fileName + ".sil", filePrefix + fileName + "Ref" + ".sil", frontend))
   }
 
-  class CustomFrontend extends SilFrontend {
-    def createVerifier(fullCmd: _root_.scala.Predef.String) = ???
+  private def parseAndCompare(testFile: String, refFile: String, frontend: MockSilFrontend): Unit = {
+    val fileRes = getClass.getResource(testFile)
+    assert(fileRes != null, s"File $testFile not found")
+    val file = Paths.get(fileRes.toURI)
 
-    def configureVerifier(args: Seq[String]) = ???
+    val fileRef = getClass.getResource(refFile)
+    assert(fileRef != null, s"File $refFile not found")
+    val fileR = Paths.get(fileRef.toURI)
 
-    def typeCheckCustom(p: PProgram): Boolean = {
-      doTypecheck(p) match {
-        case Succ(_) => true
-        case Fail(_) => false
-      }
+    var targetNode: Node = null
+    var targetRef: Node = null
+
+    frontend.translate(file) match {
+      case (Some(p), _) => targetNode = p
+      case (None, errors) => sys.error("Error occurred during translating: " + errors)
     }
 
-    def translate(silverFile: Path): (Option[Program], Seq[AbstractError]) = {
-      _verifier = None
-      _state = TranslatorState.Initialized
-
-      reset(silverFile) //
-      translate()
-
-      //println(s"_program = ${_program}") /* Option[Program], set if parsing and translating worked */
-      //println(s"_errors = ${_errors}")   /*  Seq[AbstractError], contains errors, if encountered */
-
-      (_program, _errors)
+    frontend.translate(fileR) match {
+      case (Some(p), _) => targetRef = p
+      case (None, errors) => sys.error("Error occurred during translating: " + errors)
     }
+
+    val obtainedOutput = targetNode.toString()
+    val expectedOutput = targetRef.toString()
+
+    assert(obtainedOutput == expectedOutput,
+           s"""Transformation of program $fileRes did not yield expected program $fileRef:
+              |------Got ------
+              |$obtainedOutput
+              |--- Expected ---
+              |$expectedOutput
+            """.stripMargin)
   }
-
 }
