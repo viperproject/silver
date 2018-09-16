5b6edda99b3bc57cfcea2bfb3477257be5a7ec21
Alessio Aurecchia <alessio@aurecchia.ch>
2018-09-16 17:53:43 +0200

    Remove timestamp in forgotten file.

--- src/main/scala/viper/silver/reporter/Reporter.scala
+++ src/main/scala/viper/silver/reporter/Reporter.scala
@@ -64,7 +64,7 @@ case class StdIOReporter(name: String = "stdout_reporter", timeInfo: Boolean = t
         errors.foreach(e => println(s"  ${e.readableMessage}"))
         println( s"Run with just --help for usage and options" )
 
-      case ExecutionTraceReport(timestamp, memberTraces, axioms, functionPostAxioms) =>
+      case ExecutionTraceReport(memberTraces, axioms, functionPostAxioms) =>
         println("Execution trace for the last run:")
         println(s"  Members:")
         memberTraces.foreach(t => println(s"    $t"))
