a8f923f05e057866b392696c1b147f8c8da4627e
fabio <fabio@archlinux>
2019-01-11 18:55:56 +0100

    Using a DecreasesDomain with decreasesN functions to overcome the type checker.

--- src/main/scala/viper/silver/plugin/DecreasePlugin.scala
+++ src/main/scala/viper/silver/plugin/DecreasePlugin.scala
@@ -5,30 +5,15 @@ import viper.silver.ast.pretty.PrettyPrintPrimitives
 import viper.silver.ast._
 import viper.silver.ast.pretty.FastPrettyPrinter.{ContOps, char, parens, show, space, ssep, text, toParenDoc}
 import viper.silver.parser._
+import viper.silver.verifier.{Failure, Success, VerificationResult}
 
 
 // run --printTranslatedProgram --plugin viper.silver.plugin.DecreasePlugin silver/src/test/resources/termination/basic/test.vpr
 class DecreasePlugin extends SilverPlugin
 {
 
-  /*
-
-  /** Called before any processing happened.
-    *
-    * @param input      Source code as read from file
-    * @param isImported Whether the current input is an imported file or the main file
-    * @return Modified source code
-    */
-  override def beforeParse(input: String, isImported: Boolean): String = {
-    if (!isImported){
-      "domain DecreaseDomain[T]{function decreases(t: T):Bool}\n" + input
-    }else{
-      input
-    }
-  }
-  */
-
-
+  // decreases "keyword"
+  private val DECREASES = "decreases"
 
   /** Called after parse AST has been constructed but before identifiers are resolved and the program is type checked.
     *
@@ -36,109 +21,90 @@ class DecreasePlugin extends SilverPlugin
     * @return Modified Parse AST
     */
   override def beforeResolve(input: PProgram): PProgram = {
-    // get the arguments of all decreases calls in postconditions
-    // and remove them
-    println("old:")
-    println(input)
-
-
-    val decreaseDomain = input.domains.find(d => d.idndef.name == "DecreaseDomainx").get
+    // replace all decreases (calls in postconditions)
+    // with decreasesN calls
+    // and add DecreasesDomain with all needed decreasesN functions
 
     var newFunctions = Seq[PFunction]()
 
     for(function <- input.functions){
       var posts = Seq[PExp]()
-      var args: Seq[PExp] = null;
 
       for(post <- function.posts){
-        if (post.isInstanceOf[PCall]){
-          val call = post.asInstanceOf[PCall]
-          if (call.opName == "decreases"){
-            // remove post condition and store it
-            val argsSize = call.args.length
-
-            val decreaseN = getDecreaseN(decreaseDomain, argsSize)
-
-            //posts = posts :+ post
-
-          }else{
+        post match {
+          case call: PCall =>
+            if (call.opName == DECREASES) {
+              // change call
+              val argsSize = call.args.length
+              val functionName = getDecreasesNFunction(argsSize)
+              posts = posts :+ call.copy(func = PIdnUse(functionName))
+            } else {
+              posts = posts :+ post
+            }
+          case _ =>
             posts = posts :+ post
-          }
-        }else{
-          posts = posts :+ post
         }
       }
-      val newFunction = function.copy(posts = posts)
       newFunctions = newFunctions :+ function.copy(posts = posts)
     }
 
+    val newDomain = createDecreasesDomain(getDecreasesNDomain, decreasesNFunctions.toMap)
 
-    val newDomains: Seq[PDomain] = decreaseNFunctions.values.toSeq
-
-    val newProgram = input.copy(functions = newFunctions, domains = newDomains)
-
-
-    println("New Program:")
-    println(newProgram.functions)
-    println(newProgram.domains)
-    newProgram
+    input.copy(functions = newFunctions, domains = input.domains :+ newDomain)
   }
 
-  // #arguments -> (domain name, function name)
-  val decreaseNFunctions = collection.mutable.Map[Integer, (PDomain)]()
-
-  def getDecreaseN(decreaseDomain: PDomain, argsSize: Integer): (PDomain) = {
-    if (!decreaseNFunctions.contains(argsSize)) {
-      val domainName: String = "DecreasesDomain" + argsSize
-      val functionName: String = "decreases"
-
-      val typVars = Seq.tabulate(argsSize)(i => PTypeVarDecl(PIdnDef("T" + i)))
-
-      val vars = typVars.map(t => PFormalArgDecl(PIdnDef("v" + t.idndef.name), PPrimitiv(t.idndef.name)))
-
-      val newDomainFunction = PDomainFunction(PIdnDef(functionName), vars, TypeHelper.Bool, false)(PIdnUse(domainName))
-      val newDomain = PDomain(PIdnDef(domainName), typVars, Seq(newDomainFunction), Seq())
-
-      //val newDomainFunction = decreaseDomain.funcs.head.copy(PIdnDef(functionName), vars, TypeHelper.Bool, false)(PIdnUse(domainName))
-      //val newDomain = decreaseDomain.copy(PIdnDef(domainName), typVars, Seq(newDomainFunction), Seq())
-
-      //println(decreaseDomain.funcs.head.formalArgs.head.typ.freeTypeVariables)
-
-      //val newDomainFunction = decreaseDomain.funcs.head.copy(formalArgs = vars)(decreaseDomain.funcs.head.domainName)
-      //val newDomain = decreaseDomain.copy(typVars = typVars, funcs = Seq(newDomainFunction))
-
-      decreaseNFunctions += (argsSize -> (newDomain))
-
-      (newDomain)
-    }else{
-      decreaseNFunctions(argsSize)
-    }
-  }
-
-  /*
-
-  /** Called after identifiers have been resolved but before the parse AST is translated into the normal AST.
-    *
-    * @param input Parse AST
-    * @return Modified Parse AST
+  /**
+    * All needed decreasesN functions
+    * (#arguments -> function name)
     */
-  override def beforeTranslate(input: PProgram): PProgram = {
+  private val decreasesNFunctions = collection.mutable.Map[Integer, String]()
 
-    // put arguments of decreases calls back
-    var newFunctions = Seq[PFunction]()
-
-    for (function <- input.functions){
-      val dec = pMapFunctionDecreases.get(function.idndef.name)
-      if (dec.isDefined){
-        newFunctions = newFunctions :+ function.copy(posts = function.posts :+ dec.get)
-      }else{
-        newFunctions = newFunctions :+ function
-      }
+  /**
+    * Lazy map using the decreasesNFunctions
+    * @param argsSize: number of arguments needed
+    * @return name of decrease function
+    */
+  private def getDecreasesNFunction(argsSize: Integer): String = {
+    if (!decreasesNFunctions.contains(argsSize)){
+      val newName: String = s"$$decreases${argsSize}"
+      decreasesNFunctions += (argsSize -> newName)
     }
-
-    input.copy(functions = newFunctions)
+    decreasesNFunctions(argsSize)
   }
 
+  private def getDecreasesNDomain: String ={
+    "$DecreasesDomain"
+  }
+
+  /**
+    * Creates a DecreasesDomain with type parameters
+    * @param domain name of the created domain
+    * @param functions #arguments -> function name
+    * @return the DecreasesDomain
+    */
+  private def createDecreasesDomain(domain: String, functions: Map[Integer, String]): PDomain = {
+
+    val domainIdDef = PIdnDef(domain)
+    val domainIdUse = PIdnUse(domain)
+
+    // number of type parameters needed
+    val maxArgs: Integer = functions.keySet.max
+
+    // all type parameters
+    val typeVars = Seq.tabulate(maxArgs)(i => PTypeVarDecl(PIdnDef(s"T$i")))
+
+    // list of arguments with the type parameter
+    val formalArgs: Seq[PFormalArgDecl] = typeVars.map(t => PFormalArgDecl(PIdnDef(s"v${t.idndef.name}"), PDomainType(PIdnUse(t.idndef.name), Seq())))
+
+    // all needed decreasesN functions
+    val decreasesFunctions = functions.map {
+      case (argsSize, functionName) => {
+        PDomainFunction(PIdnDef(functionName), formalArgs.take(argsSize), PPrimitiv(TypeHelper.Bool.name), false)(domainIdUse)
+      }
+    }(collection.breakOut)
+
+    PDomain(domainIdDef, typeVars, decreasesFunctions, Seq())
+  }
 
   /** Called after parse AST has been translated into the normal AST but before methods to verify are filtered.
     * In [[viper.silver.frontend.SilFrontend]] this step is confusingly called doTranslate.
@@ -149,35 +115,28 @@ class DecreasePlugin extends SilverPlugin
   override def beforeMethodFilter(input: Program): Program = {
     // get all decreases in post conditions and
     // transform them to one post condition with a DecreaseExp
-
-    // add all functions to this sequence
+    // replace decreasesN again with decreases
     var newFunctions = Seq[Function]()
+    val decreasesN = decreasesNFunctions.values.toSeq
 
-    for(function <- input.functions){
-      var posts = Seq[Exp]()  // sequence of all post conditions without decreases
-      var decs = Seq[Exp]()   // sequence of all post conditions with decreases
-
-      for(post <- function.posts){
-        if (post.isInstanceOf[Call] && post.asInstanceOf[Call].callee == "decreases"){
-          // the decrease function only has one argument (=> one subExp)
-          decs = decs :+ post.subExps.head
-          posts = posts :+ post // keep them for pretty print!
-        }else{
-          posts = posts :+ post
+    for (function <- input.functions){
+      val posts = function.posts map {
+        case c: Call => {
+          if (decreasesNFunctions.get(c.args.size).contains(c.callee)) {
+            DecreaseExp(true, c.pos, c.args)
+          }else {
+            c
+          }
         }
+        case p => p
       }
-
-      if (decs.nonEmpty){
-        // put all decreases into one DecreaseExp and add it to the postconditions
-        posts = posts :+ DecreaseExp(extensionIsPure = true, function.pos, decs)
-      }
-
-      // copy the function with the new postconditions
       newFunctions = newFunctions :+ function.copy(posts = posts)(function.pos, function.info, function.errT)
     }
 
+    val newDomains = input.domains.filterNot(d => d.name == getDecreasesNDomain)
+
     // copy the program with the new functions
-    input.copy(functions = newFunctions)(input.pos, input.info, input.errT)
+    input.copy(functions = newFunctions, domains = newDomains)(input.pos, input.info, input.errT)
   }
 
   /** Called after methods are filtered but before the verification by the backend happens.
@@ -231,7 +190,6 @@ class DecreasePlugin extends SilverPlugin
 
     newProgram.copy(domains = d, functions = newProgram.functions ++ f, methods = newProgram.methods ++ m)(newProgram.pos, newProgram.info, newProgram.errT)
   }
-  */
 }
 
 case class DecreaseExp(extensionIsPure: Boolean, pos: Position, extensionSubnodes: Seq[Exp]) extends ExtensionExp {
@@ -241,13 +199,11 @@ case class DecreaseExp(extensionIsPure: Boolean, pos: Position, extensionSubnode
   /** Pretty printing functionality as defined for other nodes in class FastPrettyPrinter.
     * Sample implementation would be text("old") <> parens(show(e)) for pretty-printing an old-expression. */
 
-
   override def info: Info = NoInfo
 
   override def errT: ErrorTrafo = NoTrafos
 
   /** Pretty printing functionality as defined for other nodes in class FastPrettyPrinter.
     * Sample implementation would be text("old") <> parens(show(e)) for pretty-printing an old-expression. */
-  // TODO: !! NO IDEA WHAT TO USE HERE?!?!
-  override def prettyPrint: PrettyPrintPrimitives#Cont = ssep(extensionSubnodes map (toParenDoc(_)), char(',') <> space)
+  override def prettyPrint: PrettyPrintPrimitives#Cont = text("decreases") <> parens(ssep(extensionSubnodes map (toParenDoc(_)), char(',') <> space))
 }
