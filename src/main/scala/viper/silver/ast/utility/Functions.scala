7be25104b406f3ab880e5bce0324fdd1b9005d38
Fabio Streun <fstreun@student.ethz.ch>
2019-03-07 18:12:53 +0100

    corrected typo, indentation, ...

--- src/main/scala/viper/silver/ast/utility/Functions.scala
+++ src/main/scala/viper/silver/ast/utility/Functions.scala
@@ -180,12 +180,10 @@ object Functions {
     *         formed cycles involves the set of functions `fs`.
     */
   def findFunctionCyclesViaPreconditions(program: Program): Map[Function, Set[Function]] = {
-    def via(func: Function): Seq[Exp] = func.pres
-    def subs(func: Function): Seq[Exp] = allSubexpressions(func)
-    findFunctionCyclesVia(program, via, subs)
+    findFunctionCyclesVia(program, func => func.pres, allSubexpressions)
   }
 
-  /** Returns all cycles formed by functions that (transitively through certain sub expressions)
+  /** Returns all cycles formed by functions that (transitively through certain subexpressions)
     * recurses via certain expressions.
     *
     * @param program The program that defines the functions to check for cycles.
@@ -196,7 +194,7 @@ object Functions {
     *         formed cycles involves the set of functions `fs`.
     */
   def findFunctionCyclesVia(program: Program, via: Function => Seq[Exp], subs: Function => Seq[Exp] = allSubexpressions)
-  :Map[Function, Set[Function]] = {
+      :Map[Function, Set[Function]] = {
     def viaSubs(entryFunc: Function)(otherFunc: Function): Seq[Exp] =
       if (otherFunc == entryFunc)
         via(otherFunc)
