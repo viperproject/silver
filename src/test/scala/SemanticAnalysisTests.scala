e066a760bc8f89ef2f0229b1aeaff6a8e4e9467e
Fábio Pakk Selmi-Dei <fabio.pakk@inf.ethz.ch>
2019-04-17 16:59:25 +0200

    Fixed issue 191: typechecker crashes on AST with sharing.

--- src/test/scala/SemanticAnalysisTests.scala
+++ src/test/scala/SemanticAnalysisTests.scala
@@ -11,33 +11,31 @@ import viper.silver.frontend._
 import viper.silver.parser._
 import viper.silver.verifier.Verifier
 
-// Test the following Viper program, with and without shared nodes in the AST.
-// method main() {
-//   assert 1 == 1
-//   assert 1 == 1
-// }
-
 class SemanticAnalysisTests extends FunSuite {
 
-  val binExp1 = PBinExp(PIntLit(1), "==", PIntLit(1))
-  val binExp2 = PBinExp(PIntLit(1), "==", PIntLit(1))
+  object frontend extends SilFrontend {
+    def configureVerifier(args: Seq[String]): SilFrontendConfig = ???
+    def createVerifier(fullCmd: String): Verifier = ???
+  }
 
-  private def testMethod(method: PMethod): Unit = {
+  // Test the following Viper program, with and without shared nodes in the AST.
+  // method m() {
+  //   assert 1 == 1
+  //   assert 1 == 1
+  // }
+
+  test("Semantic analysis in AST without shared nodes") {
+    val binExp1 = PBinExp(PIntLit(1), "==", PIntLit(1))
+    val binExp2 = PBinExp(PIntLit(1), "==", PIntLit(1))
+    val method = PMethod(PIdnDef("m"), Seq(), Seq(), Seq(), Seq(), Some(PSeqn(Seq(PSeqn(Seq(PAssert(binExp1), PSeqn(Seq(PAssert(binExp2)))))))))
     val program = PProgram(Seq(), Seq(), Seq(), Seq(), Seq(), Seq(), Seq(method), Seq())
-
-    object frontend extends SilFrontend {
-      def configureVerifier(args: Seq[String]): SilFrontendConfig = ???
-      def createVerifier(fullCmd: String): Verifier = ???
-    }
-
     assert(frontend.doSemanticAnalysis(program) === frontend.Succ(program))
   }
 
-  test("Semantic analysis in AST without shared nodes") {
-    testMethod(PMethod(PIdnDef("main"), Seq(), Seq(), Seq(), Seq(), Some(PSeqn(Seq(PSeqn(Seq(PAssert(binExp1), PSeqn(Seq(PAssert(binExp2))))))))))
-  }
-
   test("Semantic analysis in AST with shared nodes") {
-    testMethod(PMethod(PIdnDef("main"), Seq(), Seq(), Seq(), Seq(), Some(PSeqn(Seq(PSeqn(Seq(PAssert(binExp1), PSeqn(Seq(PAssert(binExp1))))))))))
+    val binExp = PBinExp(PIntLit(1), "==", PIntLit(1))
+    val method = PMethod(PIdnDef("m"), Seq(), Seq(), Seq(), Seq(), Some(PSeqn(Seq(PSeqn(Seq(PAssert(binExp), PSeqn(Seq(PAssert(binExp)))))))))
+    val program = PProgram(Seq(), Seq(), Seq(), Seq(), Seq(), Seq(), Seq(method), Seq())
+    assert(frontend.doSemanticAnalysis(program) === frontend.Succ(program))
   }
 }
