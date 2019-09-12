189f1dbf6a953a472d76ee2a1c7013e6abec523e
Fábio Pakk Selmi-Dei <fabio.pakk@inf.ethz.ch>
2019-09-12 11:20:20 +0200

    Working on issue 286: Capture-avoiding substitution with bound variables. Algorithm was redone, reasoning the scope at the place of the function or method call. Added several regression test cases.

--- src/main/scala/viper/silver/utility/Sanitizer.scala
+++ src/main/scala/viper/silver/utility/Sanitizer.scala
@@ -7,7 +7,6 @@
 package viper.silver.utility
 
 import viper.silver.ast.utility.rewriter._
-import viper.silver.ast.utility.Expressions
 import viper.silver.parser._
 import viper.silver.ast._
 
@@ -29,8 +28,11 @@ object Sanitizer {
   }
 
   // This method renames bound variables when their identifiers already exist in the same scope,
-  // therefore preventing clashes with other entities and preventing free variables to be
-  // 'captured', i.e. mistakenly considered quantified variables.
+  // therefore preventing name clashes with other entities and preventing free variables to be
+  // 'captured', i.e., mistakenly considered bound variables. At the moment, this is not used
+  // since conceptually Viper does not allow name clashes and issues a "duplicate identifier"
+  // error in such cases. But this solution remains here as a suggestion of how rule number 1
+  // could be implemented if name clashes were allowed.
   def sanitizeBoundVariables(program: PProgram): PProgram = {
 
     def scopeAt(node: PNode): Set[String] =
@@ -72,62 +74,49 @@ object Sanitizer {
   // This method replaces free variables in the expression by corresponding subexpressions.
   // If bound variables exist and their identifiers clash with the new free variables or scope,
   // then the bound variables are renamed, preventing the new free variables from being 'captured'.
-  def replaceFreeVariablesInExpression[E <: Exp](expression: E, replaces: Map[LocalVar, Exp], scope: Set[String]): E = {
+  def replaceFreeVariablesInExpression[E <: Exp](expression: E, replacements: Map[LocalVar, Exp], scope: Set[String]): E = {
 
-    def partialScope(node: Node): Set[String] = {
-      node match {
-        case scope: Scope => scope.scopedDecls.map(_.name).toSet
-        case _ => Set()
-      }
-    }
-
-    def freeVariables(exp: Exp): Set[String] = {
-      Expressions.freeVariables(exp).map(_.name)
-    }
-
-    // Free variables introduced by replacement
-    val introducedFreeVariables = replaces.values.flatMap(freeVariables(_)).toSet
-    // TODO: check if will actually be introduced, because not all replacements should occur
-
-    case class Context(replaces: Map[LocalVar, Exp], scope: Set[String], renames: Map[String, String] = Map())
+    case class Context(scope: Set[String], replacements: Map[LocalVar, Exp], renaming: Map[String, String] = Map())
 
     val result = StrategyBuilder.RewriteNodeAndContext[Node, Context]({
       case (forall: Forall, c) => {
 
         var scope = c.scope
-        var renames = c.renames
+        var renaming = c.renaming
 
         // Check if bound variables clash with scope
         val bounds = forall.variables.map(_.name).toSet
         val intersection = scope & bounds
+        scope = scope ++ bounds
 
-        // Define how clashing bound variables will be renamed to fresh names
+        // Plan the renaming of clashing bound variables to fresh names
         intersection.foreach {
           name =>
-            val freshName = fresh(name, scope ++ bounds)
-            renames += ((name, freshName))
+            val freshName = fresh(name, scope)
+            renaming += ((name, freshName))
             scope += freshName
         }
 
-        // Enforce that a variable is never both renamed and replaced
-        assert((renames.keys.toSet & c.replaces.keys.map(_.name).toSet).isEmpty)
+        // Enforce that a variable is never both replaced and renamed
+        assert((c.replacements.keys.map(_.name).toSet & renaming.keys.toSet).isEmpty)
 
-        (forall, c.copy(renames = renames, scope = scope ++ partialScope(forall)))
+        (forall, c.copy(scope = scope, renaming = renaming))
       }
 
-      case (lv: LocalVarDecl, c) if c.renames.contains(lv.name) =>
-        (LocalVarDecl(c.renames(lv.name), lv.typ)(lv.pos, lv.info, lv.errT), c)
+      // Rename bound variable in definition
+      case (lv: LocalVarDecl, c) if c.renaming.contains(lv.name) =>
+        (LocalVarDecl(c.renaming(lv.name), lv.typ)(lv.pos, lv.info, lv.errT), c)
 
-      case (lv: LocalVar, c) if c.renames.contains(lv.name) =>
-        (LocalVar(c.renames(lv.name), lv.typ)(lv.pos, lv.info, lv.errT), c)
+      // Rename bound variable in use
+      case (lv: LocalVar, c) if c.renaming.contains(lv.name) =>
+        (LocalVar(c.renaming(lv.name), lv.typ)(lv.pos, lv.info, lv.errT), c)
 
-      case (lv: LocalVar, c) if c.replaces.contains(lv) =>
-        (c.replaces(lv), c.copy(replaces = Map()))
+      // Replace free variable by expression
+      case (lv: LocalVar, c) if c.replacements.contains(lv) =>
+        (c.replacements(lv), c.copy(replacements = Map(), renaming = Map()))
 
-      case (s: Scope, c) => (s, c.copy(scope = c.scope ++ partialScope(s)))
+    }, Context(scope, replacements)).execute(expression).asInstanceOf[E] //? Remove cast
 
-    }, Context(replaces, scope ++ introducedFreeVariables)).execute(expression).asInstanceOf[E] //? remove cast
-
-    result //?
+    result //? Remove 'result' as it is here just for debugging
   }
 }
