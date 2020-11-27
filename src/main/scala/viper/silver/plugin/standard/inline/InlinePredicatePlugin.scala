fa6539e9168f18e665307edc037e0d368d489be0
Jonathan Chan <jcxz@cs.ubc.ca>
2020-11-27 12:24:33 -0800

    Mostly using Strategys to traverse various AST structures.

--- src/main/scala/viper/silver/plugin/standard/inline/InlinePredicatePlugin.scala
+++ src/main/scala/viper/silver/plugin/standard/inline/InlinePredicatePlugin.scala
@@ -1,7 +1,6 @@
 package viper.silver.plugin.standard.inline
 
 import viper.silver.ast.utility.ViperStrategy
-import viper.silver.ast.utility.rewriter.Traverse
 import viper.silver.ast.Program
 import viper.silver.plugin.{ParserPluginTemplate, SilverPlugin}
 
@@ -10,15 +9,12 @@ class InlinePredicatePlugin extends SilverPlugin with ParserPluginTemplate with 
   private[this] val InlinePredicateKeyword = "inline"
 
   override def beforeVerify(input: Program): Program = {
-    val declaredMethods = input.methods
-    val methodsWithInlinedPreds = declaredMethods.map(inlinePredicates(_, input))
-    val rewrittenMethods = methodsWithInlinedPreds.map(removeUnfoldFold(_, input))
-
-    // TODO: actually rewrite program with methods with expanded predicates
-    val rewrittenProgram = ViperStrategy.Slim({
-      case program: Program =>
-        program.copy(methods = rewrittenMethods)(program.pos, program.info, program.errT)
-    }, Traverse.BottomUp).execute(input)
-    rewrittenProgram
+    ViperStrategy.Slim({
+      case program@Program(_, _, _, _, methods, _) =>
+        program.copy(methods = methods.map {
+          rewriteMethod(_, program)
+        })(program.pos, program.info, program.errT)
+    }) // .recurseFunc(???) // TODO: Do we need to call this first?
+      .execute(input)
   }
 }
--- src/main/scala/viper/silver/plugin/standard/inline/InlineRewrite.scala
+++ src/main/scala/viper/silver/plugin/standard/inline/InlineRewrite.scala
@@ -1,79 +1,84 @@
 package viper.silver.plugin.standard.inline
 
-import viper.silver.ast.{Exp, Fold, LocalVar, Method, PredicateAccess, Program, Stmt, Unfold}
+import scala.collection.mutable
+import viper.silver.ast._
+import viper.silver.ast.utility.ViperStrategy
 
 trait InlineRewrite {
 
-  def inlinePredicates(method: Method, program: Program): Method = {
-    val expandedPres = method.pres.map { pre =>
-      expandPredicate(pre, program).fold(pre)(expandedPred => expandedPred)
-    }
-    val expandedPosts = method.posts.map { post =>
-      expandPredicate(post, program).fold(post)(expandedPred => expandedPred)
-    }
+  /**
+    * Rewrites a method by expanding the predicates in its pre- and postconditions,
+    * then by removing the corresponding unfold and fold statements in the body.
+    * Note that we only remove unfolds for predicates from the preconditions,
+    * and folds for predicates from the postconditions.
+    * (I think that's how it works.)
+    */
+  def rewriteMethod(method: Method, program: Program): Method = {
+    val (expandedPres, expandablePrePredsIds) = method.pres
+      .map { expandPredicates(_, method, program) }
+      .unzip
+    val (expandedPosts, expandablePostPredsIds) = method.posts
+      .map { expandPredicates(_, method, program) }
+      .unzip
+    val expandablePrePredIds = expandablePrePredsIds.fold(Set())(_ ++ _)
+    val expandablePostPredIds = expandablePostPredsIds.fold(Set())(_ ++ _)
+    val rewrittenBody = method.body
+      .map { removeFoldUnfolds(_, expandablePrePredIds, expandablePostPredIds) }
     method.copy(name = method.name,
-      formalArgs = method.formalArgs,
-      formalReturns = method.formalReturns,
-      pres = expandedPres,
-      posts = expandedPosts,
-      body = method.body
-    )(pos = method.pos, info = method.info, errT = method.errT)
+        formalArgs = method.formalArgs,
+        formalReturns = method.formalReturns,
+        pres = expandedPres,
+        posts = expandedPosts,
+        body = rewrittenBody
+      )(pos = method.pos, info = method.info, errT = method.errT)
   }
 
-  def removeUnfoldFold(method: Method, program: Program): Method = {
-    val expandablePredicateIds = getExpandablePredicateIds(method.pres, program)
-    val rewrittenBody = method.body.map { body =>
-      val bodyWithRemovedUnfolds = removeUnfolds(body.ss, expandablePredicateIds)
-      val bodyWithRemovedUnfoldFolds = removeFolds(bodyWithRemovedUnfolds, expandablePredicateIds)
-      body.copy(ss = bodyWithRemovedUnfoldFolds, scopedDecls = body.scopedDecls)(
-        pos = body.pos, info = body.info, errT = body.errT
-      )
-    }
-    method.copy(name = method.name,
-      formalArgs = method.formalArgs,
-      formalReturns = method.formalReturns,
-      pres = method.pres,
-      posts = method.posts,
-      body = rewrittenBody
-    )(pos = method.pos, info = method.info, errT = method.errT)
+  /**
+    * Expands all predicates to their bodies in given expression.
+    * 
+    * TODO: Expand only some specified predicates, not all
+    *
+    * @param expr The expression whose predicates will be expanded
+    * @param method The method containing the expression, used to determine locally-scoped variables
+    * @param program The program containing the expression, used to expand predicates
+    * @return The expression with expanded predicates and a set of their names
+    */
+  private[this] def expandPredicates(expr: Exp, method: Method, program: Program): (Exp, Set[String]) = {
+    // Forgive me deities of functional programming for I sin
+    val expandablePredicates = mutable.Set[String]()
+    val expandedExpr = ViperStrategy.CustomContext[Set[String]]({
+      case (expr, args) => expr match {
+        case pred: PredicateAccess =>
+          val body = pred.predicateBody(program, args)
+          if (body.isDefined) expandablePredicates += pred.predicateName
+          (body.getOrElse(pred), args)
+        case quant: QuantifiedExp => ???
+          // TODO: Continue traversing quant, but with context (args ++ quant.scopedDecls.map { _.name }.toSet)
+      }
+    }, method.scopedDecls.map { _.name }.toSet)
+      // .recurseFunc(???) // TODO: Do we need to call this first?
+      .execute(expr)
+    (expandedExpr, expandablePredicates.toSet)
   }
 
-  private[this] def expandPredicate(expr: Exp, program: Program): Option[Exp] =
-    expr match {
-      case pred: PredicateAccess => getPredicateBody(pred, program)
-      case _ => None
-    }
-
-  private[this] def getPredicateBody(pred: PredicateAccess, program: Program): Option[Exp] = {
-    def extractLocalVarIds(predicate: PredicateAccess): Set[String] =
-      predicate.args.map {
-        case arg: LocalVar => arg.name
-        case _ => ""
-      }.filter(_.nonEmpty).toSet
-
-    val args = extractLocalVarIds(pred)
-    pred.predicateBody(program, args)
-  }
-
-  private[this] def getExpandablePredicateIds(exprs: Seq[Exp], program: Program): Seq[String] =
-    exprs.collect {
-      case exp: PredicateAccess if getPredicateBody(exp, program).nonEmpty => exp.predicateName
-  }
-
-  private[this] def removeUnfolds(bodyStmts: Seq[Stmt], predicatesToRemove: Seq[String]): Seq[Stmt] =
-    bodyStmts.filter {
-      case unfoldExp: Unfold =>
-        val unfoldPredName = unfoldExp.acc.loc.predicateName
-        predicatesToRemove.contains(unfoldPredName)
-      case _ => false
-    }
-
-  private[this] def removeFolds(bodyStmts: Seq[Stmt], predicatesToRemove: Seq[String]): Seq[Stmt] = {
-    bodyStmts.filter {
-      case foldExp: Fold =>
-        val foldPredName = foldExp.acc.loc.predicateName
-        predicatesToRemove.contains(foldPredName)
-      case _ => false
-    }
+  /**
+    * Removes given predicate unfolds and folds from statement.
+    * 
+    * TODO: How to specify to Slim that the type to traverse is Stmt in general, not only Seqn?
+    *
+    * @param stmt A Seqn whose statments will be traversed
+    * @param unfoldPreds A set of the string names of the precondition predicates to not unfold
+    * @param foldPreds A set of the string names of the postcondition predicates to not fold
+    * @return The Seqn with all above unfolds and folds removed
+    */
+  private[this] def removeFoldUnfolds(stmts: Seqn, unfoldPreds: Set[String], foldPreds: Set[String]): Seqn = {
+    val noop: Stmt = Assert(TrueLit.apply()())() // TODO: Is there some other way of getting rid of a statement?
+    ViperStrategy.Slim({
+      case fold@Fold(PredicateAccessPredicate(PredicateAccess(_, name), _)) =>
+        if (foldPreds(name)) noop else fold
+      case unfold@Unfold(PredicateAccessPredicate(PredicateAccess(_, name), _)) =>
+        if (unfoldPreds(name)) noop else unfold
+    }) // .recurseFunc(???) // TODO: Do we need to call this first?
+      .execute(stmts)
   }
 }
