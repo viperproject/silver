ecf747b305c6032db77e15623d4050caaff32d41
Jonathan Chan <jcxz@cs.ubc.ca>
2020-12-01 16:23:31 -0800

    You may not like it, but *this* is peak monad

--- src/main/scala/viper/silver/plugin/standard/inline/InlineRewrite.scala
+++ src/main/scala/viper/silver/plugin/standard/inline/InlineRewrite.scala
@@ -45,7 +45,7 @@ trait InlineRewrite {
     val expandedExpr = ViperStrategy.Context[Set[String]]({
       case exp@(PredicateAccessPredicate(pred, _), ctxt) =>
         val body = pred.predicateBody(program, ctxt.c)
-        val isInUnfolding = ctxt.parentOption.fold(false)(_.isInstanceOf[Unfolding])
+        val isInUnfolding = ctxt.parentOption.exists({case Unfolding(_, _) => true})
         if (body.isDefined && !isInUnfolding) {
           expandablePredicates += pred.predicateName
           (body.get, ctxt)
