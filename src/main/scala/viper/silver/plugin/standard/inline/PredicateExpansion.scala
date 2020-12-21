406441d31b39a52a155756bc3d959ee301722555
Jonathan Chan <jcxz@cs.ubc.ca>
2020-12-20 19:26:25 -0800

    Fix incorrect permission propagation (should be multiplicative)

--- src/main/scala/viper/silver/plugin/standard/inline/PredicateExpansion.scala
+++ src/main/scala/viper/silver/plugin/standard/inline/PredicateExpansion.scala
@@ -1,7 +1,7 @@
 package viper.silver.plugin.standard.inline
 
 import viper.silver.ast.utility.ViperStrategy
-import viper.silver.ast.{Exp, FieldAccessPredicate, PredicateAccessPredicate}
+import viper.silver.ast._
 import viper.silver.ast.utility.rewriter.Traverse
 
 trait PredicateExpansion {
@@ -17,17 +17,19 @@ trait PredicateExpansion {
     */
   def propagatePermission(maybeBody: Option[Exp], originalPerm: Exp): Option[Exp] =
     maybeBody.map { body =>
-      ViperStrategy.Slim({
-        case predAccessPred: PredicateAccessPredicate =>
-          predAccessPred.copy(
-            loc = predAccessPred.loc,
-            perm = originalPerm
-          )(pos = predAccessPred.pos, info = predAccessPred.info, errT = predAccessPred.errT)
-        case fieldAccessPred: FieldAccessPredicate =>
-          fieldAccessPred.copy(
-            loc = fieldAccessPred.loc,
-            perm = originalPerm
-          )(pos = fieldAccessPred.pos, info = fieldAccessPred.info, errT = fieldAccessPred.errT)
-      }, Traverse.TopDown).execute[Exp](body)
+      ViperStrategy.CustomContext[Exp]({
+        case (predAcc@PredicateAccessPredicate(_, perm), outerPerm) =>
+          val propagatedPerm = PermMul(perm, outerPerm)(perm.pos, perm.info, perm.errT)
+          val modifiedPredAcc = predAcc.copy(
+              perm = propagatedPerm
+            )(predAcc.pos, predAcc.info, predAcc.errT)
+          (modifiedPredAcc, propagatedPerm)
+        case (fieldAcc@FieldAccessPredicate(_, perm), outerPerm) =>
+          val propagatedPerm = PermMul(perm, outerPerm)(perm.pos, perm.info, perm.errT)
+          val modifiedFieldAcc = fieldAcc.copy(
+              perm = propagatedPerm
+            )(fieldAcc.pos, fieldAcc.info, fieldAcc.errT)
+          (modifiedFieldAcc, propagatedPerm)
+      }, originalPerm, Traverse.TopDown).execute[Exp](body)
     }
 }
