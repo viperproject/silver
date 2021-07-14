38ae16190dbb47505eeab6837c9b38faa332340d
Fabian Bösiger <boesiger.fabian@outlook.com>
2021-07-14 15:44:27 +0200

    add requested changes

--- src/main/scala/viper/silver/cfg/silver/SilverCfg.scala
+++ src/main/scala/viper/silver/cfg/silver/SilverCfg.scala
@@ -83,47 +83,55 @@ class SilverCfg(val blocks: Seq[SilverBlock], val edges: Seq[SilverEdge], val en
                     exit: Option[SilverBlock] = exit): SilverCfg =
     SilverCfg(blocks, edges, entry, exit)
 
+  /**
+    * Recursively finds the next join point to a branch point.
+    *
+    * @param queueInit Initializes the BFS queue with blocks that are yet to be visited.
+    * @param visitedInit Initialized list of visited nodes.
+    * @param loopHeadsSeen All the loop heads that were visited so far.
+    * @param getNext function which returns the successor nodes.
+    * @return (jp, m) where jp is the next join point,
+    * and m maps all branch points which have been already found
+    * to their join points.
+    */
   private def findJoinPoint(queueInit: Iterable[SilverBlock],
                             visitedInit: Iterable[SilverBlock],
                             // We never enqueue loopHeads which we already have seen.
                             // This would lead to non-termination.
                             loopHeadsSeen: Iterable[SilverBlock],
                             getNext: SilverBlock => Iterable[SilverBlock])
-  : (Option[SilverBlock], mutable.Map[SilverBlock, SilverBlock]) = {
+                            : (Option[SilverBlock], mutable.Map[SilverBlock, SilverBlock]) = {
 
     var queue = mutable.Queue.from(queueInit)
-    var visited: Vector[SilverBlock] = Vector.from(visitedInit)
+    var visited: mutable.Set[SilverBlock] = mutable.Set.from(visitedInit)
     val map = mutable.Map[SilverBlock, SilverBlock]()
-    var loopHeads: Vector[SilverBlock] = Vector.from(loopHeadsSeen)
+    var loopHeads: mutable.Set[SilverBlock] = mutable.Set.from(loopHeadsSeen)
 
     while (queue.nonEmpty) {
       val curr = queue.dequeue()
-      val visitNext = curr match {
-        case curr =>
-          if (!visited.contains(curr)) {
-            visited :+= curr
-            if (curr.isInstanceOf[SilverLoopHeadBlock]) {
-              loopHeads :+= curr
-            }
-            getNext(curr) match {
-              case out @ Seq() => out
-              case out @ Seq(_) => out
-              case out @ Seq(_, _) =>
-                val (joinPoint, innerMap) =
-                  findJoinPoint(out.filter(!loopHeads.contains(_)), Seq(curr), loopHeads, getNext)
-                map ++= innerMap
-                joinPoint match {
-                  case Some(joinPoint) =>
-                    map += curr -> joinPoint
-                  case None => ()
-                }
-                joinPoint
-              case _ => sys.error("At most two outgoing edges expected.")
-            }
-          } else {
-            return (Some(curr), map)
+      val visitNext =
+        if (!visited.contains(curr)) {
+          visited += curr
+          if (curr.isInstanceOf[SilverLoopHeadBlock]) {
+            loopHeads += curr
           }
-      }
+          getNext(curr) match {
+            case out @ Seq() => out
+            case out @ Seq(_) => out
+            case out @ Seq(_, _) =>
+              val (joinPoint, innerMap) =
+                findJoinPoint(out.filter(!loopHeads.contains(_)), Seq(curr), loopHeads, getNext)
+              map ++= innerMap
+              joinPoint foreach {
+                jp => map += curr -> jp
+              }
+              joinPoint
+            case _ => sys.error("At most two outgoing edges expected.")
+          }
+        } else {
+          return (Some(curr), map)
+        }
+
 
       // Avoid re-visiting already seen loop heads.
       queue = queue.enqueueAll(visitNext.iterator.filter(!loopHeads.contains(_)))
@@ -136,7 +144,7 @@ class SilverCfg(val blocks: Seq[SilverBlock], val edges: Seq[SilverEdge], val en
     *
     * @return Mapping from all branch points to join points.
     */
-  lazy val joinPoints: mutable.Map[SilverBlock, SilverBlock] = {
+  lazy val joinPoints: collection.Map[SilverBlock, SilverBlock] = {
     val (jp, map) = findJoinPoint(Seq(entry), Seq.empty, Seq.empty, successors)
     assert(jp.isEmpty)
     map
