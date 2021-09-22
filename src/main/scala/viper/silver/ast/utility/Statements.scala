62d3b70fdf6df9b75c94b530747c467d84ea8696
Jérôme Dohrau <jerome.dohrau@inf.ethz.ch>
2021-09-22 13:44:23 +0200

    Do not list locally declared variables as undeclared
    Signed-off-by: Jérôme Dohrau <jerome.dohrau@inf.ethz.ch>

--- src/main/scala/viper/silver/ast/utility/Statements.scala
+++ src/main/scala/viper/silver/ast/utility/Statements.scala
@@ -11,20 +11,19 @@ import viper.silver.ast._
 /** Utility methods for statements. */
 object Statements {
   /** An empty statement. */
-  val EmptyStmt = Seqn(Nil, Nil)()
+  val EmptyStmt: Seqn = Seqn(Nil, Nil)()
 
   /**
    * Returns a list of all actual statements contained in a given statement.  That
    * is, all statements except `Seqn`, including statements in the body of loops, etc.
    */
-  def children(s: Stmt): Seq[Stmt] = {
+  def children(s: Stmt): Seq[Stmt] =
     s match {
       case While(_, _, body) => Seq(s) ++ children(body)
       case If(_, thn, els) => Seq(s) ++ children(thn) ++ children(els)
       case Seqn(ss, _) => ss flatMap children
       case _ => List(s)
     }
-  }
 
   /**
    * Returns a list of all undeclared local variables used in this statement.
@@ -32,30 +31,41 @@ object Statements {
    * types, an exception is thrown.
    */
   def undeclLocalVars(s: Stmt): Seq[LocalVar] = {
-    def extractLocal(n: Node, decls: Seq[LocalVarDecl]) = n match {
-      case l: LocalVar => decls.find(_.name == l.name) match {
-        case None => List(l)
-        case Some(d) if d.typ != l.typ =>
-          sys.error("Local variable " + l.name + " is declared with type " + d.typ + " but used with type " + l.typ + ".")
+    def extractLocal(n: Node, decls: Seq[LocalVarDecl]) =
+      n match {
+        case l: LocalVar => decls.find(_.name == l.name) match {
+          case None => List(l)
+          case Some(d) if d.typ != l.typ =>
+            sys.error(s"Local variable ${l.name} is declared with type ${d.typ} but used with type ${l.typ}.")
+          case _ => Nil
+        }
         case _ => Nil
       }
-      case _ => Nil
-    }
+
     def combineLists(s1: Seq[LocalVar], s2: Seq[LocalVar]) = {
       for (l1 <- s1; l2 <- s2) {
         if (l1.name == l2.name && l1.typ != l2.typ) {
-          sys.error("Local variable " + l1.name + " is used with different types " + l1.typ + " and " + l2.typ)
+          sys.error(s"Local variable ${l1.name} is used with different types ${l1.typ} and ${l2.typ}.")
         }
       }
       (s1 ++ s2).distinct
     }
+
     def addDecls(n: Node, decls: Seq[LocalVarDecl]) = n match {
-      case QuantifiedExp(v, _) => decls ++ v
-      case _ => decls
+      case QuantifiedExp(variables, _) =>
+        // add quantified variables
+        decls ++ variables
+      case Seqn(_, scoped) =>
+        // add variables defined in scope
+        decls ++ scoped.collect { case variable: LocalVarDecl => variable }
+      case _ =>
+        decls
     }
-    def combineResults(n: Node, decls: Seq[LocalVarDecl], localss: Seq[Seq[LocalVar]]) = {
-      localss.fold(extractLocal(n, decls))(combineLists)
+
+    def combineResults(n: Node, decls: Seq[LocalVarDecl], locals: Seq[Seq[LocalVar]]) = {
+      locals.fold(extractLocal(n, decls))(combineLists)
     }
+
     s.reduceWithContext(Nil, addDecls, combineResults)
   }
 
