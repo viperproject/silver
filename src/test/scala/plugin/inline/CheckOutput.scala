a5f24606b23dc1a7f249bbed6641a43c349c1644
David Ewert <dewert10@gmail.com>
2021-11-17 17:15:52 -0800

    Added an easy way to check the output of inlining

---
+++ src/test/scala/plugin/inline/CheckOutput.scala
@@ -0,0 +1,33 @@
+
+
+
+import TestHelpers.{FileComparisonHelper, MockSilFrontend}
+
+package plugin.inline{
+  import org.scalatest.FunSuite
+  import viper.silver.ast.Program
+  import viper.silver.plugin.standard.inline.InlinePredicatePlugin
+
+  import java.nio.file.Paths
+
+  class CheckOutput extends FunSuite with FileComparisonHelper {
+    test("Performance_BinomialHeap") {
+      val fileName = "mutrec"
+
+      val frontend = new MockSilFrontend
+      val fileRes = getClass.getResource(s"$fileName.vpr")
+      assert(fileRes != null, s"File $fileName not found")
+      val file = Paths.get(fileRes.toURI)
+      val src: Program = frontend.translate(file) match {
+        case (Some(p), _) => p
+        case (None, errors) => fail("Problem with program: " + errors)
+      }
+      val inliner = frontend.plugins.plugins.collectFirst({
+        case p: InlinePredicatePlugin => p;
+      }).get
+      println(src)
+      val res = inliner.beforeVerify(src)
+      println(res)
+    }
+  }
+}
