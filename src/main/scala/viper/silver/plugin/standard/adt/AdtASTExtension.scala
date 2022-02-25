9fc8beeb3e16695cc73c156e6d9dfc89544cea1b
Alessandro Maissen <alessandro.maissen@bluewin.ch>
2022-02-25 12:45:21 +0100

    Add an AST node for the ADT type
    -Create a class for the ADT type that hold the necessary information we need in a later step to encode the ADT type as a domain type

--- src/main/scala/viper/silver/plugin/standard/adt/AdtASTExtension.scala
+++ src/main/scala/viper/silver/plugin/standard/adt/AdtASTExtension.scala
@@ -6,7 +6,7 @@
 
 package viper.silver.plugin.standard.adt
 
-import viper.silver.ast.{AnyLocalVarDecl, Declaration, ErrorTrafo, ExtensionMember, Info, LocalVarDecl, NoInfo, NoPosition, NoTrafos, Node, Position, TypeVar}
+import viper.silver.ast.{AnyLocalVarDecl, Declaration, ErrorTrafo, ExtensionMember, ExtensionType, Info, LocalVarDecl, NoInfo, NoPosition, NoTrafos, Node, Position, Type, TypeVar}
 
 /**
   * This class represents a user-defined ADT.
@@ -39,6 +39,42 @@ case class AdtConstructor(name: String, formalArgs: Seq[AnyLocalVarDecl])
   val scopedDecls: Seq[Declaration] = formalArgs.filter(p => p.isInstanceOf[LocalVarDecl]).asInstanceOf[Seq[LocalVarDecl]]
 
   override def extensionSubnodes: Seq[Node] = formalArgs
+}
+
+/**
+  * This class represents an user-defined ADT type. See also the companion object below, which allows passing a
+  * Adt - this should be used in general for creation (so that typeParameters is guaranteed to
+  * be set correctly)
+  *
+  * @param adtName The name of the underlying adt.
+  * @param partialTypVarsMap Maps type parameters to (possibly concrete) types. May not map all
+  *                          type parameters, may even be empty.
+  * @param typeParameters The type variables of the ADT type.
+  */
+case class AdtType(adtName: String, partialTypVarsMap: Map[TypeVar, Type])
+                  (val typeParameters: Seq[TypeVar]) extends ExtensionType {
+
+  /**
+    * Map each type parameter `A` to `partialTypVarsMap(A)`, if defined, otherwise to `A` itself.
+    * `typVarsMap` thus contains a mapping for each type parameter.
+    */
+  val typVarsMap: Map[TypeVar, Type] =
+    typeParameters.map(tp => tp -> partialTypVarsMap.getOrElse(tp, tp)).to(implicitly)
+
+  /**
+    * Takes a mapping of type variables to types and substitutes all
+    * occurrences of those type variables with the corresponding type.
+    */
+  override def substitute(typVarsMap: Map[TypeVar, Type]): Type = ???
+
+  /** Is this a concrete type (i.e. no uninstantiated type variables)? */
+  override def isConcrete: Boolean = typVarsMap.values.forall(_.isConcrete)
+
+}
+
+object AdtType{
+  def apply(adt: Adt, typVarsMap: Map[TypeVar, Type]): AdtType =
+    AdtType(adt.name, typVarsMap)(adt.typVars)
 }
 
 
