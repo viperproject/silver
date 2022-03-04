5faa7c00c7405db57966ed757c75fb4b9a45228c
Alessandro Maissen <alessandro.maissen@bluewin.ch>
2022-03-04 17:43:11 +0100

    Add an AST node for an ADT constructor application
    -Additionally, missing methods of AdtType are implemented

--- src/main/scala/viper/silver/plugin/standard/adt/AdtASTExtension.scala
+++ src/main/scala/viper/silver/plugin/standard/adt/AdtASTExtension.scala
@@ -6,9 +6,11 @@
 
 package viper.silver.plugin.standard.adt
 
+import viper.silver.ast._
 import viper.silver.ast.pretty.FastPrettyPrinter.{ContOps, braces, brackets, char, defaultIndent, line, nest, nil, parens, show, showVars, space, ssep, text}
 import viper.silver.ast.pretty.PrettyPrintPrimitives
-import viper.silver.ast._
+import viper.silver.ast.utility.Consistency
+import viper.silver.verifier.{ConsistencyError, Failure, VerificationResult}
 
 /**
   * This class represents a user-defined ADT.
@@ -44,7 +46,7 @@ case class Adt(name: String, constructors: Seq[AdtConstructor], typVars: Seq[Typ
   * @param adtName the name corresponding of the corresponding ADT
   */
 case class AdtConstructor(name: String, formalArgs: Seq[AnyLocalVarDecl])
-                     (val pos: Position, val info: Info, typ: AdtType, val adtName : String, val errT: ErrorTrafo)
+                     (val pos: Position, val info: Info, val typ: AdtType, val adtName : String, val errT: ErrorTrafo)
   extends ExtensionMember {
 
   override def getMetadata:Seq[Any] = {
@@ -88,6 +90,10 @@ object AdtConstructor {
 case class AdtType(adtName: String, partialTypVarsMap: Map[TypeVar, Type])
                   (val typeParameters: Seq[TypeVar]) extends ExtensionType {
 
+  override lazy val check: Seq[ConsistencyError] = if(!(typeParameters.toSet == typVarsMap.keys.toSet)) {
+    Seq(ConsistencyError(s"${typeParameters.toSet} doesn't equal ${typVarsMap.keys.toSet}", NoPosition))
+  } else Seq()
+
   /**
     * Map each type parameter `A` to `partialTypVarsMap(A)`, if defined, otherwise to `A` itself.
     * `typVarsMap` thus contains a mapping for each type parameter.
@@ -99,7 +105,11 @@ case class AdtType(adtName: String, partialTypVarsMap: Map[TypeVar, Type])
     * Takes a mapping of type variables to types and substitutes all
     * occurrences of those type variables with the corresponding type.
     */
-  override def substitute(typVarsMap: Map[TypeVar, Type]): Type = ???
+  override def substitute(newTypVarsMap: Map[TypeVar, Type]): Type = {
+    assert (typVarsMap.keys.toSet equals this.typeParameters.toSet)
+    val newTypeMap = typVarsMap.map(kv=>kv._1 -> kv._2.substitute(newTypVarsMap))
+    AdtType(adtName, newTypeMap)(typeParameters)
+  }
 
   /** Is this a concrete type (i.e. no uninstantiated type variables)? */
   override def isConcrete: Boolean = typVarsMap.values.forall(_.isConcrete)
@@ -125,6 +135,58 @@ case class AdtType(adtName: String, partialTypVarsMap: Map[TypeVar, Type])
 object AdtType{
   def apply(adt: Adt, typVarsMap: Map[TypeVar, Type]): AdtType =
     AdtType(adt.name, typVarsMap)(adt.typVars)
+}
+
+
+/**
+  * This class represents a user-defined adt constructor application. See also the companion object below, which allows passing a
+  * Adt constructor - this should be used in general for creation (so that typ is guaranteed to
+  * be set correctly)
+  *
+  * @param name The name of the ADT constructor
+  * @param args A sequence of expressions passed as arguments to the constructor
+  * @param typVarMap Maps type parameters to (possibly concrete) types. May not map all
+  *                  type parameters, may even be empty.
+  * @param typ The return type of the constructor
+  * @param adtName The corresponding ADT name
+  */
+case class AdtConstructorApp(name: String, args: Seq[Exp], typVarMap: Map[TypeVar, Type])
+                        (val pos: Position, val info: Info, override val typ: Type, val adtName:String, val errT: ErrorTrafo)
+  extends ExtensionExp {
+  override lazy val check : Seq[ConsistencyError] = args.flatMap(Consistency.checkPure)
+
+  override def prettyPrint: PrettyPrintPrimitives#Cont = {
+    if (typVarMap.nonEmpty)
+      // Type may be under-constrained, so to be safe we explicitly print out the type.
+      parens(text(name) <> parens(ssep(args map show, char (',') <> space)) <> char(':') <+> show(typ))
+    else
+      text(name) <> parens(ssep(args map show, char (',') <> space))
+  }
+
+  override def extensionIsPure: Boolean = true
+
+  override def extensionSubnodes: Seq[Node] = args ++ typVarMap.keys ++ typVarMap.values
+
+  override def verifyExtExp(): VerificationResult = {
+    assert(assertion = false, "AdtConstructorApp: verifyExtExp has not been implemented.")
+    Failure(Seq(ConsistencyError("AdtConstructorApp: verifyExtExp has not been implemented.", pos)))
+  }
+
+  override def withChildren(children: Seq[Any], pos: Option[(Position, Position)], forceRewrite: Boolean): AdtConstructorApp.this.type = {
+    if (!forceRewrite && this.children == children && pos.isEmpty)
+      this
+    else {
+      assert(children.length == 3, s"AdtConstructorApp : expected length 3 but got ${children.length}")
+      val first = children.head.asInstanceOf[String]
+      val second = children(1).asInstanceOf[Seq[Exp]]
+      val third = children(2).asInstanceOf[Map[TypeVar, Type]]
+      AdtConstructorApp(first, second, third)(this.pos, this.info, this.typ, this.adtName, this.errT).asInstanceOf[this.type]
+    }
+  }
+}
+object AdtConstructorApp {
+  def apply(constructor : AdtConstructor, args: Seq[Exp], typVarMap: Map[TypeVar, Type])(pos: Position = NoPosition, info: Info = NoInfo, errT: ErrorTrafo = NoTrafos) : AdtConstructorApp =
+    AdtConstructorApp(constructor.name, args, typVarMap)(pos, info, constructor.typ.substitute(typVarMap), constructor.adtName, errT)
 }
 
 
