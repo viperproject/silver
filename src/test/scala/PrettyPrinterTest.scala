e49f84dbe003d1649d8b5be318023db7868eeac5
Zack Grannan <zgrannan@protonmail.com>
2022-07-05 13:19:42 -0700

    Pretty-printing test for negative int literals

--- src/test/scala/PrettyPrinterTest.scala
+++ src/test/scala/PrettyPrinterTest.scala
@@ -32,11 +32,18 @@ class PrettyPrinterTest extends AnyFunSuite with Matchers {
   test("Parsing a pretty-printed 'And' expression should return the same expression") {
     compareToParsed(and(and(tt, tt), tt))
   }
+
   test("Parsing a pretty-printed 'Or' expression should return the same expression") {
     compareToParsed(or(or(tt, tt), tt))
   }
 
+  test("Parsing pretty-printed '-5' should return the same expression") {
+    compareToParsed(IntLit(-5)())
+  }
+
   private def compareToParsed(exp: Exp): Unit = {
+    val declStmt = LocalVarDeclStmt(LocalVarDecl("tmp", exp.typ)())()
+    val assign = LocalVarAssign(declStmt.decl.localVar, exp)()
     compareToParsed(
       Program(
         Nil,
@@ -52,7 +59,7 @@ class PrettyPrinterTest extends AnyFunSuite with Matchers {
             Nil,
             Some(
               Seqn(
-                List(Assert(exp)()),
+                List(declStmt, assign),
                   Nil
                 )()
               )
@@ -71,9 +78,13 @@ class PrettyPrinterTest extends AnyFunSuite with Matchers {
     fw.write(printed)
     fw.close()
 
+    def getExp(program: Program) = {
+      program.methods.head.body.get.ss(1)
+    }
+
     frontend.translate(file.toPath) match {
       case (Some(result), _) =>
-        assert(result == prog, s"${prog} did not match ${result}")
+        assert(getExp(result) == getExp(prog), s"${prog} did not match ${result}")
       case (None, errors) =>
         sys.error("Error occurred during translating: " + errors)
     }
