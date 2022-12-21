9b916808f244781e11521fd0300eb94d1b39ce36
Zack Grannan <zgrannan@protonmail.com>
2022-12-21 12:52:28 -0800

    WIP

--- src/main/scala/viper/silver/plugin/standard/permprime/PermPrimePlugin.scala
+++ src/main/scala/viper/silver/plugin/standard/permprime/PermPrimePlugin.scala
@@ -2,11 +2,13 @@ package viper.silver.plugin.standard.permprime
 
 import fastparse._
 import viper.silver.ast.pretty.PrettyPrintPrimitives
+import viper.silver.ast.utility.Visitor
 import viper.silver.ast.utility.rewriter.{StrategyBuilder, Traverse}
-import viper.silver.ast.{ErrorTrafo, Exp, ExtensionStmt, Info, LocalVar, MethodCall, Node, Position, Program, Seqn, Stmt}
-import viper.silver.parser.{FastParser, NameAnalyser, PCurPerm, PExp, PExtender, PIdnUse, PNode, PResourceAccess, PSeqn, PStmt, PTypeSubstitution, Translator, TypeChecker}
+import viper.silver.ast.{CurrentPerm, ErrorTrafo, Exp, ExtensionExp, ExtensionStmt, Info, LocalVar, MethodCall, NoInfo, NoPosition, NoTrafos, Node, Perm, PermExp, Position, Program, ResourceAccess, Seqn, Stmt, Type}
+import viper.silver.parser.{FastParser, NameAnalyser, PCurPerm, PExp, PExtender, PIdnUse, PMethod, PNode, PProgram, PResourceAccess, PSeqn, PStmt, PTypeSubstitution, Translator, TypeChecker}
 import viper.silver.plugin.SilverPlugin
 import viper.silver.parser.FastParserCompanion.whitespace
+import viper.silver.verifier.VerificationResult
 
 import scala.annotation.unused
 
@@ -29,9 +31,17 @@ case class PMethodCallPrime(
     MethodCall(t.findMethod(method), args map t.exp, ts)(this.pos._1 /* TODO: Is this right? */)
   }
 }
+case class PPermPrime (res: PResourceAccess)(val pos: (Position, Position)) extends PExtender with PExp {
 
-case class PPermPrime (res: PResourceAccess)(val pos: (Position, Position)) extends PExtender with PExp
-{
+  override def translateExp(t: Translator): Exp = {
+    t.exp(PCurPerm(res)(pos))
+  }
+
+  override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
+    t.checkTopTyped(res, None)
+    None
+  }
+  override def getSubnodes(): Seq[PNode] = List(res)
   override def typeSubstitutions: collection.Seq[PTypeSubstitution] = ???
 
   override def forceSubstitution(ts: PTypeSubstitution): Unit = ???
@@ -57,6 +67,20 @@ class PermPrimePlugin(@unused reporter: viper.silver.reporter.Reporter,
     FP(PermPrimeKeyword ~ parens(resAcc)).map{
       case (pos, r) => PPermPrime(r)(pos)
     }
+  }
+
+  override def beforeTranslate(input: PProgram): PProgram = {
+    println("[PermPrime] enter beforeTranslate")
+    StrategyBuilder.Slim[PNode] {
+      case method: PMethod =>
+        method.copy(
+          pres = method.pres.filterNot { c =>
+              (Visitor.find(c, (e: PNode) => e.subnodes) {
+                case _: PPermPrime => true
+              }).isDefined
+            }
+        )(method.pos)
+    }.execute(input)
   }
 
   override def beforeParse(input: String, isImported: Boolean): String = {
