31987bc914f1f772691d2bae8a90a92aebef0453
Dspil <dennisspiliopoylos@gmail.com>
2023-08-05 13:59:56 +0200

    kati

--- src/main/scala/viper/silver/reporter/Reporter.scala
+++ src/main/scala/viper/silver/reporter/Reporter.scala
@@ -211,9 +211,6 @@ case class BenchmarkingReporter(name: String = "benchmarking_reporter") extends 
   // That map has the following special keys:
   // -1: where the total time is stored
   // -2: where the number of instances is stored
-  // -3: where a negative value means we have to overrule following instances on this accumulator
-  //     and a positive means that we can keep opening instances (useful for benchmarking
-  //     recursive methods)
   private var _accumulators: scala.collection.mutable.Map[String, scala.collection.mutable.Map[Int, Long]] = Map.empty
   //private var _rec: scala.collection.mutable.Seq[(String
   private var _previous_phase: Long = _initial_time
@@ -224,25 +221,17 @@ case class BenchmarkingReporter(name: String = "benchmarking_reporter") extends 
         val t = System.currentTimeMillis()
         println(s"[Benchmarking] Phase $phase at ${t - _initial_time} ms and took ${t - _previous_phase} ms")
         _previous_phase = t
-      case BenchmarkingAccumulator(accum, id, rec) =>
+      case BenchmarkingAccumulator(accum, id) =>
         if (_accumulators contains accum) {
           if (_accumulators(accum) contains id) {
             _accumulators(accum)(-1) += (System.currentTimeMillis() - _accumulators(accum)(id))
             _accumulators(accum)(-2) += 1
             _accumulators(accum) remove id
-            _accumulators(accum)(-3) = 1
           } else {
-            if (_accumulators(accum)(-3) > 0) {
-              _accumulators(accum)(id) = System.currentTimeMillis()
-              if (rec)
-                _accumulators(accum)(-3) = -1
-            } else println(_accumulators(accum).keySet)
+            _accumulators(accum)(id) = System.currentTimeMillis()
           }
         } else {
-          if (rec)
-            _accumulators(accum) = Map((-3, -1), (-1, 0), (-2, 0), (id, System.currentTimeMillis()))
-          else
-            _accumulators(accum) = Map((-3, 1), (-1, 0), (-2, 0), (id, System.currentTimeMillis()))
+          _accumulators(accum) = Map((-1, 0), (-2, 0), (id, System.currentTimeMillis()))
         }
       case BenchmarkingReport(msg, accum) if _accumulators contains accum =>
         println(s"[Benchmarking] Accumulated time for '$msg' took ${_accumulators(accum)(-1)} ms across ${_accumulators(accum)(-2)} instances")
