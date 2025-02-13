1b5809b9fed2fdb2b042d628bbcf8344e63b5e8b
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-02-13 12:29:07 +0100

    Fix compilation

--- src/main/scala/viper/silver/cfg/CfgTest.scala
+++ src/main/scala/viper/silver/cfg/CfgTest.scala
@@ -35,7 +35,7 @@ object CfgTest {
   }
 
   private def parse(input: String, file: Path): Option[PProgram] = {
-    val program = new FastParser().parse(input, file, false)
+    val program = new FastParser().parse(input, file, expandMacros = false)
     if (program.errors.forall(_.isInstanceOf[ParseWarning])) Some(program)
     else None
   }
